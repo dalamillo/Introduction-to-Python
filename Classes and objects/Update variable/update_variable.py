d01ff0e8b1749c492644076de9f9f2db61881e9b
dalamillo <d4vid.4lvi@gmail.com>
2026-09-10 11:47:05 +0200

    Ex: update_variable

--- Classes and objects/Update variable/update_variable.py
+++ Classes and objects/Update variable/update_variable.py
@@ -7,10 +7,10 @@ class Car:
 
 
 car1 = Car()
-car2 = ???
+car2 = Car()
 
 car1.color = "blue"
-car2.color = ???
+car2.color = "red"
 
-print('Call description() method of car1')
-print('Call description() method of car2')
+print(Car.description(car1))
+print(Car.description(car2))
