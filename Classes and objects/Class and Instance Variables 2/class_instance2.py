135553bb356c1285b0eb802d2c7ded8167a773c7
dalamillo <d4vid.4lvi@gmail.com>
2026-09-14 13:00:24 +0200

    Ex: class_instance2

--- Classes and objects/Class and Instance Variables 2/class_instance2.py
+++ Classes and objects/Class and Instance Variables 2/class_instance2.py
@@ -1,15 +1,14 @@
 class City:
-    # TODO: add a mutable class variable (all_cities list) to store all added city names
+    all_cities = []# TODO: add a mutable class variable (all_cities list) to store all added city names
 
     def __init__(self, name, population, country):
         self.name = name
         self.population = population
         self.country = country
-        # TODO: call add_city method to add the city name to the list upon object initialization
+        self.add_city()# TODO: call add_city method to add the city name to the list upon object initialization
 
     def add_city(self):
-        pass
-        # TODO: implement a method that will append a city name to the all_cities list. Delete the pass statement!
+        self.all_cities.append(self.name)# TODO: implement a method that will append a city name to the all_cities list. Delete the pass statement!
 
 
 if __name__ == '__main__':
@@ -17,4 +16,4 @@ if __name__ == '__main__':
     boston = City('Boston', 689326, 'USA')
     beijing = City('Beijing', 21540000, 'China')
 
-    print(malaga.all_cities)  # This should print "['Malaga', 'Boston', 'Beijing']".
+    print(beijing.all_cities)  # This should print "['Malaga', 'Boston', 'Beijing']".
