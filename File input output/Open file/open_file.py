3dd995bec56214ca4bea671f645b38d857eaadbb
dalamillo <d4vid.4lvi@gmail.com>
2026-09-17 10:09:58 +0200

    Ex: open_file bugged solution

--- File input output/Open file/open_file.py
+++ File input output/Open file/open_file.py
@@ -1,12 +1,10 @@
 with open('input.txt', 'r') as my_file:
-    # Some action performed with the file, the read() method explained later.
     print(my_file.read(), '\n')
 
 
-# Open the file input1.txt in read mode using the with statement
+with open('input1.txt', 'r') as file:
     outfile_name = file.readline()
 
-
-outfile = open(???, 'w')  # Opening the file in write mode (using the `w` argument)
-outfile.write('Hello World')  # Writing to the file, the write() method is explained later.
-# Close the output file!
+outfile = open(outfile_name, 'w')
+outfile.write('Hello World')
+outfile.close()
